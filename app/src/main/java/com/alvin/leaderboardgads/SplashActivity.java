9ce6d18deacf5f0a93b62c0962a141c08d9167ae
Alvin-Karanja <alvin2@protonmail.com>
2020-09-07 10:40:59 +0300

    Set splash screen

---
+++ app/src/main/java/com/alvin/leaderboardgads/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.alvin.leaderboardgads;
+
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.logging.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Handler()
+                .postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    }
+                }, 2000);
+    }
+    @Override
+    public void onBackPressed() {}
+}
