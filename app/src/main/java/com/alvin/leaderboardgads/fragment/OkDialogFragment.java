afef51b1f9f63e122c26d47f0297b7519f067013
Alvin-Karanja <alvin2@protonmail.com>
2020-09-07 11:23:51 +0300

    Created OkDialogFragment

---
+++ app/src/main/java/com/alvin/leaderboardgads/fragment/OkDialogFragment.java
@@ -0,0 +1,37 @@
+package com.alvin.leaderboardgads.fragment;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import static java.security.AccessController.getContext;
+
+public class OkDialogFragment extends DialogFragment {
+    public static final String ARG_MESSAGE = "message";
+
+    public OkDialogFragment() {
+        Bundle defaultArgs = new Bundle();
+        defaultArgs.putString(ARG_MESSAGE, "");
+        setArguments(defaultArgs);
+    }
+
+    public static OkDialogFragment newInstance(String message) {
+        OkDialogFragment dialog = new OkDialogFragment();
+        dialog.getArguments().putString(ARG_MESSAGE, message);
+        return dialog;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getContext())
+                .setMessage(getArguments().getString(ARG_MESSAGE))
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+
+}
